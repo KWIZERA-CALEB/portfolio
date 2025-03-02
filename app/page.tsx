453d57c3c74703eca7cd1ccdcc11437d08140e57
KWIZERA-CALEB <kwizeracaleb91@gmail.com>
2025-03-02 17:23:19 +0200

    fix: fixed sentry build issues

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,8 @@
 import { navItems } from "@/data";
 
 import Hero from "@/components/Hero";
-// import Grid from "@/components/Grid";
 import Footer from "@/components/Footer";
 import TechStack from "@/components/TechStack";
-import Approach from "@/components/Approach";
-import Experience from "@/components/Experience";
 import RecentProjects from "@/components/RecentProjects";
 import { FloatingNav } from "@/components/ui/FloatingNavbar";
 
@@ -17,12 +14,8 @@ const Home = () => {
       <div className="max-w-7xl w-full">
         <FloatingNav navItems={navItems} />
         <Hero />
-        {/* <Grid /> */}
         <TechStack />
         <RecentProjects />
-        {/* <Clients /> */}
-        {/* <Experience /> */}
-        {/* <Approach /> */}
         <Footer />
       </div>
     </main>
