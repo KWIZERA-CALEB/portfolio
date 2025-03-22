d1e82e65b2fc33d30cc37e9a6c96f73a648401de
KWIZERA-CALEB <kwizeracaleb91@gmail.com>
2025-03-22 15:37:17 +0200

    added projects

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -4,6 +4,7 @@ import { FaLocationArrow } from "react-icons/fa6";
 
 import { projects } from "@/data";
 import { PinContainer } from "./ui/Pin";
+import Link from 'next/link'
 
 const RecentProjects = () => {
   return (
@@ -66,9 +67,11 @@ const RecentProjects = () => {
                 </div>
 
                 <div className="flex justify-center items-center">
-                  <span className="flex text-sm font-medium cursor-pointer text-purple">
-                    Check Live Site
-                  </span>
+                  <Link href={item.link} target='_blank'>
+                    <span className="flex text-sm font-medium cursor-pointer text-purple">
+                      Check Live Site
+                    </span>
+                  </Link>
                   <FaLocationArrow className="ms-3" color="#CBACF9" />
                 </div>
               </div>
